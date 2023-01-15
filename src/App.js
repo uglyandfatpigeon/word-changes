d37c70845f8cf2eb414803d745d13c0db775d22a
sg0307760 <dominika.niziolek@sabre.com>
2023-01-15 22:29:19 +0100

    creating environment and loading gltf-s

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
 import './App.css'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import Box from './components/Box'
+import { OrbitControls, Stats, Environment } from '@react-three/drei'
+// import Box from './components/Box'
+import { useControls } from 'leva'
+import { useMemo } from 'react'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+function Model() {
+  const gltf = useLoader(GLTFLoader, './model/treasure_chest.gltf')
+  return <primitive object={gltf.scene} scale={'10'} position={[0, -8, 0]} />
+}
 
 function App() {
+  // const options = useMemo(() => {
+  //   return {
+  //     color: { value: 'blue' }
+  //   }
+  // }, [])
+  //
+  // const pA = useControls('A', options)
+  // const pB = useControls('B', options)
+
   return (
     <Canvas camera={{ position: [0, 3, 3], near: 0.025 }}>
       <OrbitControls />
       <pointLight position={[10, 10, 10]} />
 
-      <Box position={[-0.75, 0.5, 0]} name="A" color="purple" />
-      <Box position={[0.75, 0.5, 0]} name="B" color="red" />
+      <Environment files="./img/interior.hdr" background />
+      <Model />
 
-      <gridHelper />
-      <axesHelper args={[5]} />
+      {/*<Box position={[-0.75, 0.5, 0]} name="A" color={pA.color} />*/}
+      {/*<Box position={[0.75, 0.5, 0]} name="B" color={pB.color} />*/}
+
+      {/*<gridHelper />*/}
+      {/*<axesHelper args={[5]} />*/}
+      <Stats />
     </Canvas>
   )
 }
--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
 
 const Box = (props) => {
+  const ref = useRef()
+  const [hovered, setHover] = useState(false)
+  const [rotate, setRotate] = useState(false)
+
+  useFrame((_, delta) => {
+    if (rotate) {
+      ref.current.rotation.x += delta
+      ref.current.rotation.y += 0.5 * delta
+    }
+  })
+
   return (
-    <mesh {...props}>
+    <mesh
+      {...props}
+      ref={ref}
+      scale={hovered ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onPointerDown={() => setRotate(!rotate)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}>
       <boxGeometry />
       <meshPhongMaterial color={props.color} />
     </mesh>
